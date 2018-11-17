1adcd7d450541724edf9b7fca7ee183b4af40087
Christopher Rackauckas <Contact@ChrisRackauckas.com>
2018-11-17 12:29:46 -0800

    work towards weaving

---
+++ NonStiffODE/linear_wpd.jl
@@ -0,0 +1,112 @@
+
+using DifferentialEquations, DiffEqDevTools, Plots, ODEInterfaceDiffEq, ODE
+gr()
+# 2D Linear ODE
+function f(du,u,p,t)
+  for i in 1:length(u)
+    du[i] = 1.01*u[i]
+  end
+end
+function f_analytic(u₀,p,t)
+  u₀*exp(1.01*t)
+end
+tspan = (0.0,10.0)
+prob = ODEProblem(ODEFunction(f,analytic=f_analytic),rand(100,100),tspan)
+
+abstols = 1.0./10.0.^(3:13)
+reltols = 1.0./10.0.^(0:10);
+
+
+setups = [Dict(:alg=>DP5())
+          Dict(:alg=>ode45())
+          Dict(:alg=>dopri5())
+          Dict(:alg=>ARKODE(Sundials.Explicit(),etable=Sundials.DORMAND_PRINCE_7_4_5))
+          Dict(:alg=>Tsit5())]
+names = ["OrdinaryDiffEq";"ODE";"ODEInterface";"Sundials ARKODE";"OrdinaryDiffEq Tsit5"]
+wp = WorkPrecisionSet(prob,abstols,reltols,setups;names=names,save_everystep=false)
+plot(wp)
+
+
+setups = [Dict(:alg=>DP5(),:dense=>false)
+          Dict(:alg=>ode45(),:dense=>false)
+          Dict(:alg=>dopri5()) # dense=false by default: no nonlinear interpolation
+          Dict(:alg=>ARKODE(Sundials.Explicit(),etable=Sundials.DORMAND_PRINCE_7_4_5),:dense=>false)
+          Dict(:alg=>Tsit5(),:dense=>false)]
+names = ["OrdinaryDiffEq";"ODE";"ODEInterface";"Sundials ARKODE";"OrdinaryDiffEq Tsit5"]
+wp = WorkPrecisionSet(prob,abstols,reltols,setups;names=names)
+plot(wp)
+
+
+setups = [Dict(:alg=>DP5())
+          Dict(:alg=>ode45())
+          Dict(:alg=>dopri5())
+          Dict(:alg=>ARKODE(Sundials.Explicit(),etable=Sundials.DORMAND_PRINCE_7_4_5),:dense=>false)
+          Dict(:alg=>Tsit5())]
+names = ["OrdinaryDiffEq";"ODE";"ODEInterface";"Sundials ARKODE";"OrdinaryDiffEq Tsit5"]
+wp = WorkPrecisionSet(prob,abstols,reltols,setups;names=names)
+plot(wp)
+
+
+setups = [Dict(:alg=>DP5())
+          Dict(:alg=>BS3())
+          Dict(:alg=>BS5())
+          Dict(:alg=>Tsit5())]
+wp = WorkPrecisionSet(prob,abstols,reltols,setups;save_everystep=false)
+plot(wp)
+
+
+setups = [Dict(:alg=>DP5())
+          Dict(:alg=>Vern6())
+          Dict(:alg=>TanYam7())
+          Dict(:alg=>Vern7())
+          Dict(:alg=>Vern8())
+          Dict(:alg=>DP8())
+          Dict(:alg=>Vern9())]
+wp = WorkPrecisionSet(prob,abstols,reltols,setups;save_everystep=false)
+plot(wp)
+
+
+using LSODA
+setups = [Dict(:alg=>DP5())
+          Dict(:alg=>Vern7())
+          Dict(:alg=>dop853())
+          Dict(:alg=>ode78())
+          Dict(:alg=>odex())
+          Dict(:alg=>lsoda())
+          Dict(:alg=>ddeabm())
+          Dict(:alg=>ARKODE(Sundials.Explicit(),order=8))
+          Dict(:alg=>CVODE_Adams())]
+wp = WorkPrecisionSet(prob,abstols,reltols,setups;save_everystep=false)
+plot(wp)
+
+
+setups = [Dict(:alg=>DP5())
+          #Dict(:alg=>ode45())
+          Dict(:alg=>Tsit5())]
+wp = WorkPrecisionSet(prob,abstols,reltols,setups;error_estimate=:L2,dense_errors=true)
+plot(wp)
+
+
+setups = [Dict(:alg=>DP5())
+          Dict(:alg=>Vern7())
+          #Dict(:alg=>ode78())
+          ]
+wp = WorkPrecisionSet(prob,abstols,reltols,setups;error_estimate=:L2,dense_errors=true)
+plot(wp)
+
+
+using GeometricIntegratorsDiffEq
+abstols = 1.0./10.0.^(3:13)
+reltols = 1.0./10.0.^(0:10);
+dts = [1,1/2,1/4,1/10,1/20,1/40,1/60,1/80,1/100,1/140,1/240]
+setups = [Dict(:alg=>DP5())
+          Dict(:alg=>ode45())
+          Dict(:alg=>dopri5())
+          Dict(:alg=>GIERK4(),:dts=>dts)
+          Dict(:alg=>RK4(),:dts=>dts)
+          Dict(:alg=>Tsit5())]
+names = ["DifferentialEquations";"ODE";"ODEInterface";"GeometricIntegrators RK4";"DifferentialEquations RK4";"DifferentialEquations Tsit5"]
+wp = WorkPrecisionSet(prob,abstols,reltols,setups;names=names,
+                      save_everystep=false,verbose=false)
+plot(wp)
+
--- src/DiffEqBenchmarks.jl
+++ src/DiffEqBenchmarks.jl
@@ -1,3 +1,24 @@
 module DiffEqBenchmarks
 
+using Weave
+
+repo_directory = joinpath(@__DIR__,"..")
+
+function weave_file(folder,file)
+  println("File: $file")
+  tmp = joinpath(repo_directory,folder,file)
+  #weave(tmp,doctype="pandoc",out_path=:pwd)
+  println("Building Script")
+  tangle(tmp;out_path=joinpath(repo_directory,folder))
+  println("Building HTML")
+  weave(tmp,doctype = "md2html",out_path=joinpath(repo_directory,folder))
+  println("Building PDF")
+  weave(tmp,doctype="md2pdf",out_path=joinpath(repo_directory,folder))
+end
+
+function weave_all()
+  foreach(weave_file,
+          file for file in readdir("tutorials") if endswith(file, ".jmd"))
+end
+
 end
