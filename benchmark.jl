0a725726e0a62de00d94d880502c7712ae310771
Chris de Graaf <me@cdg.dev>
2020-07-20 11:13:42 -0500

    Finish renaming DiffEqBenchmarks -> SciMLBenchmarks

--- benchmark.jl
+++ benchmark.jl
@@ -1,10 +1,10 @@
-using DiffEqBenchmarks
+using SciMLBenchmarks
 if length(ARGS) == 1
     println("Benchmarking the $(ARGS[1]) folder")
-    DiffEqBenchmarks.weave_folder(ARGS[1])
+    SciMLBenchmarks.weave_folder(ARGS[1])
 elseif length(ARGS) == 2
     println("Benchmarking $(ARGS[1])/$(ARGS[2])")
-    DiffEqBenchmarks.weave_file(ARGS[1],ARGS[2])
+    SciMLBenchmarks.weave_file(ARGS[1],ARGS[2])
 else
     error("Only 1 or 2 arguments allowed!")
 end
