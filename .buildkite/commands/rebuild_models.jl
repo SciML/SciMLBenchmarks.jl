3656fc68292a5b6a1a6ed39e6a178d2b0c192393
Venkateshprasad <vensadk@gmail.com>
2021-04-14 10:05:41 +0530

    no library

--- .buildkite/commands/rebuild_models.jl
+++ .buildkite/commands/rebuild_models.jl
@@ -8,7 +8,7 @@ split_path = split(model, "/")
 file = split_path[end]
 folder = join(split_path[1:end-1], "/")
 
-project_abspath = joinpath(repo_dir, "library", model)
+project_abspath = joinpath(repo_dir, model)
 
 @info("debugging!", model, file, folder, project_abspath, isfile(project_abspath), occursin(".jmd", file))
 
