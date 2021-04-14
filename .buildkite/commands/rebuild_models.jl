3b6e1dfa0a3515c9ecd3acefbaa8baec7aed7812
Venkateshprasad <vensadk@gmail.com>
2021-04-14 09:59:34 +0530

    @info everything

--- .buildkite/commands/rebuild_models.jl
+++ .buildkite/commands/rebuild_models.jl
@@ -6,7 +6,11 @@ repo_dir = dirname(dirname(@__DIR__))
 model = ARGS[1]
 folder, file = split(model, "/")
 
+
 project_abspath = joinpath(repo_dir, "library", model)
+
+@info("debugging!", model, file, folder, project_abspath, isfile(project_abspath), occursin(".jmd", file))
+
 if !isfile(project_abspath) || !occursin(".jmd", file)
     error("Invalid file $file")
 end
