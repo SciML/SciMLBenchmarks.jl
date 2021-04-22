4fcfe622f05792f8640c02639fdc986980e7d188
Venkateshprasad <32921645+ven-k@users.noreply.github.com>
2021-04-22 12:30:27 +0530

    Add @info

--- .github/workflows/update.jl
+++ .github/workflows/update.jl
@@ -3,22 +3,26 @@ Pkg.add(["Git", "GitHub", "Dates"])
 using Git, GitHub, Dates
 
 gh_token = ARGS[1]
-auth = GitHub.authenticate(gh_token)
+myauth = GitHub.authenticate(gh_token)
+
+(@isdefined myauth) ? @info("Authentication token is found...") : @info("Coudn't find the authentication token")
 
 const git = Git.git()
 date = Dates.format(now(), "yyyy-mm-dd")
 benchpath = joinpath(@__DIR__, "..", "..", "benchmarks")
 
 # Get all the open PRs and their number
-gh_prs = GitHub.pull_requests("SciML/SciMLBenchmarks"; auth=auth)
+gh_prs = GitHub.pull_requests("SciML/SciMLBenchmarks"; auth=myauth)
 prs = Dict{String, Int64}()
 for i in 1:length(gh_prs[1])
     prs[gh_prs[1][i].head.ref] = gh_prs[1][i].number
 end
 
 # Get all the branches from the repo
-gh_branches = GitHub.branches("SciML/SciMLBenchmarks"; auth=auth)
+gh_branches = GitHub.branches("SciML/SciMLBenchmarks"; auth=myauth)
 branches = [gh_branches[1][i].name for i in 1:length(gh_branches[1])]
+
+@info("PRs and branches", prs, branches)
 
 for dir in readdir(benchpath)
     model_dir = joinpath(benchpath, dir)
@@ -49,9 +53,11 @@ for dir in readdir(benchpath)
                 "head"  => "$(dir)",
                 "base"  => "master"
             )
-            GitHub.create_pull_request("SciML/SciMLBenchmarks"; params=params, auth=auth)
+            @info("Creating a pull request from head: ", dir) 
+            GitHub.create_pull_request("SciML/SciMLBenchmarks"; params=params, auth=myauth)
         else
-            GitHub.update_pull_request("SciML/SciMLBenchmarks", prs[dir]; auth=auth)
+            @info("Updating the pull request numbered: ", prs[dir])
+            GitHub.update_pull_request("SciML/SciMLBenchmarks", prs[dir]; auth=myauth)
         end
     end
 end
