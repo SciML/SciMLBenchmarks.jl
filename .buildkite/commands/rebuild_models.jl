8a4fa363297f46cf0b817f3051550e5420642746
Elliot Saba <staticfloat@gmail.com>
2021-04-26 14:50:26 -0700

    Update rebuild_models.jl

--- .buildkite/commands/rebuild_models.jl
+++ .buildkite/commands/rebuild_models.jl
@@ -1,6 +1,17 @@
 #!/usr/bin/env julia
 
 using Pkg
+
+# Set to clone everything via SSH (if it can't be found in the package server)
+# so we authenticate via SSH key to GitHub for our private packages/registries
+Pkg.setprotocol!(protocol="ssh")
+
+# Add our registries
+Pkg.Registry.add([
+    RegistrySpec(name="General"),
+    RegistrySpec(url="https://github.com/JuliaComputing/JuliaSimRegistry")
+])
+
 Pkg.instantiate()
 using SciMLBenchmarks
 
