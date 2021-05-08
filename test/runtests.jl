cc4abfcee7f55d5be8be72060f382c1de5d1742f
Elliot Saba <staticfloat@gmail.com>
2021-05-08 00:06:21 +0000

    Move `test.jmd` into its own project folder
    This allows us to test it more effectively, as we can upload it to the
    website and such.

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,10 +2,10 @@ using SciMLBenchmarks, Test
 
 @testset "weave_file" begin
     benchmarks_dir = joinpath(dirname(@__DIR__), "benchmarks")
-    SciMLBenchmarks.weave_file(benchmarks_dir, "test.jmd")
+    SciMLBenchmarks.weave_file(joinpath(benchmarks_dir, "Testing"), "test.jmd")
 
-    @test isfile(joinpath(dirname(@__DIR__), "script", "benchmarks", "test.jl"))
-    @test isfile(joinpath(dirname(@__DIR__), "html", "benchmarks", "test.html"))
-    @test isfile(joinpath(dirname(@__DIR__), "markdown", "benchmarks", "test.md"))
-    @test isfile(joinpath(dirname(@__DIR__), "pdf", "benchmarks", "test.pdf"))
+    @test isfile(joinpath(dirname(@__DIR__), "script", "Testing", "test.jl"))
+    @test isfile(joinpath(dirname(@__DIR__), "html", "Testing", "test.html"))
+    @test isfile(joinpath(dirname(@__DIR__), "markdown", "Testing", "test.md"))
+    @test isfile(joinpath(dirname(@__DIR__), "pdf", "Testing", "test.pdf"))
 end
