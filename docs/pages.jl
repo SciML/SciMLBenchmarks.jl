91253f37d97aa8b377b70527099ccd9367699cb0
Christopher Rackauckas <accounts@chrisrackauckas.com>
2023-08-08 16:51:50 -0400

    Update pages.jl

--- docs/pages.jl
+++ docs/pages.jl
@@ -40,7 +40,7 @@ end
 # The result is in alphabetical order, change to the wanted order
 
 permute!(pages,
-    [1, 11, 7, 14, 20, 4, 5, 10, 6, 12, 15, 21, 13, 19, 8, 18, 3, 9, 16, 17, 2]
+    [1, 11, 8, 14, 20, 4, 5, 10, 6, 12, 15, 21, 13, 19, 7, 18, 3, 9, 16, 17, 2]
 )
 
 names = [
