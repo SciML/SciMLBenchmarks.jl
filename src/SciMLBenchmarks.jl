1e9a8a6ff4d7f3b783b0fb93c9a2f5637fe1f643
Arnav Kapoor <kapoorarnav43@gmail.com>
2025-07-05 14:43:40 +0530

    formatted using JuliaFormatter

--- src/SciMLBenchmarks.jl
+++ src/SciMLBenchmarks.jl
@@ -2,105 +2,107 @@ module SciMLBenchmarks
 
 using Weave, Pkg, IJulia, InteractiveUtils, Markdown
 
-repo_directory = joinpath(@__DIR__,"..")
+repo_directory = joinpath(@__DIR__, "..")
 
-macro subprocess(ex, wait=true)
-  quote
-    local project = Pkg.project().path
-    local ex_str = $(esc(sprint(Base.show_unquoted, ex)))
-    run(`$(Base.julia_cmd()) --project=$(project) -e "$(ex_str)"`; wait=$(wait))
-  end
-end
-
-function weave_file(folder,file,build_list=(:script,:github))
-  target = joinpath(folder, file)
-  @info("Weaving $(target)")
-
-  if isfile(joinpath(folder, "Project.toml")) && build_list != (:notebook,)
-    @info("Instantiating", folder)
-    Pkg.activate(folder)
-    Pkg.instantiate()
-    Pkg.build()
-  end
-
-  args = Dict{Symbol,String}(:folder=>folder,:file=>file)
-  if :script ∈ build_list
-    println("Building Script")
-    dir = joinpath(repo_directory,"script",basename(folder))
-    mkpath(dir)
-    tangle(target; out_path=dir)
-  end
-  if :html ∈ build_list
-    println("Building HTML")
-    dir = joinpath(repo_directory,"html",basename(folder))
-    mkpath(dir)
-    weave(target,doctype = "md2html",out_path=dir,args=args,fig_ext=".svg")
-  end
-  if :pdf ∈ build_list
-    println("Building PDF")
-    dir = joinpath(repo_directory,"pdf",basename(folder))
-    mkpath(dir)
-    try
-      weave(target,doctype="md2pdf",out_path=dir,args=args)
-    catch ex
-      @warn "PDF generation failed" exception=(ex, catch_backtrace())
+macro subprocess(ex, wait = true)
+    quote
+        local project = Pkg.project().path
+        local ex_str = $(esc(sprint(Base.show_unquoted, ex)))
+        run(`$(Base.julia_cmd()) --project=$(project) -e "$(ex_str)"`; wait = $(wait))
     end
-  end
-  if :github ∈ build_list
-    println("Building Github Markdown")
-    dir = joinpath(repo_directory,"markdown",basename(folder))
-    mkpath(dir)
-    weave(target,doctype = "github",out_path=dir,args=args)
-  end
-  if :notebook ∈ build_list
-    println("Building Notebook")
-    dir = joinpath(repo_directory,"notebook",basename(folder))
-    mkpath(dir)
-    Weave.convert_doc(target,joinpath(dir,file[1:end-4]*".ipynb"))
-  end
 end
 
-function weave_all(build_list=(:script,:github))
-  for folder in readdir(joinpath(repo_directory,"benchmarks"))
-    folder == "test.jmd" && continue
-    weave_folder(joinpath(repo_directory,"benchmarks",folder),build_list)
-  end
-end
+function weave_file(folder, file, build_list = (:script, :github))
+    target = joinpath(folder, file)
+    @info("Weaving $(target)")
 
-function weave_folder(folder, build_list=(:script,:github))
-  weave_files = String[]
-  priorities = Int[]
-  for file in readdir(folder)
-    # Skip non-`.jmd` files
-    endswith(file, ".jmd") || continue
-    push!(weave_files, file)
-    weave_doc = Weave.WeaveDoc(joinpath(folder, file))
-    push!(priorities, get(weave_doc.header, "priority", 0))
-  end
-
-  weave_files = weave_files[sortperm(priorities; rev=true)]
-
-  for file in weave_files
-    try
-      @eval @subprocess begin
-        using SciMLBenchmarks
-        SciMLBenchmarks.weave_file($folder, $file, $build_list)
-      end
-    catch e
-      @show folder, file
-      @error(e)
+    if isfile(joinpath(folder, "Project.toml")) && build_list != (:notebook,)
+        @info("Instantiating", folder)
+        Pkg.activate(folder)
+        Pkg.instantiate()
+        Pkg.build()
     end
-  end
+
+    args = Dict{Symbol,String}(:folder=>folder, :file=>file)
+    if :script ∈ build_list
+        println("Building Script")
+        dir = joinpath(repo_directory, "script", basename(folder))
+        mkpath(dir)
+        tangle(target; out_path = dir)
+    end
+    if :html ∈ build_list
+        println("Building HTML")
+        dir = joinpath(repo_directory, "html", basename(folder))
+        mkpath(dir)
+        weave(target, doctype = "md2html", out_path = dir, args = args, fig_ext = ".svg")
+    end
+    if :pdf ∈ build_list
+        println("Building PDF")
+        dir = joinpath(repo_directory, "pdf", basename(folder))
+        mkpath(dir)
+        try
+            weave(target, doctype = "md2pdf", out_path = dir, args = args)
+        catch ex
+            @warn "PDF generation failed" exception=(ex, catch_backtrace())
+        end
+    end
+    if :github ∈ build_list
+        println("Building Github Markdown")
+        dir = joinpath(repo_directory, "markdown", basename(folder))
+        mkpath(dir)
+        weave(target, doctype = "github", out_path = dir, args = args)
+    end
+    if :notebook ∈ build_list
+        println("Building Notebook")
+        dir = joinpath(repo_directory, "notebook", basename(folder))
+        mkpath(dir)
+        Weave.convert_doc(target, joinpath(dir, file[1:(end-4)]*".ipynb"))
+    end
 end
 
-function bench_footer(folder=nothing, file=nothing)
-    display(md"""
-    ## Appendix
+function weave_all(build_list = (:script, :github))
+    for folder in readdir(joinpath(repo_directory, "benchmarks"))
+        folder == "test.jmd" && continue
+        weave_folder(joinpath(repo_directory, "benchmarks", folder), build_list)
+    end
+end
 
-    These benchmarks are a part of the SciMLBenchmarks.jl repository, found at: <https://github.com/SciML/SciMLBenchmarks.jl>.
-    For more information on high-performance scientific machine learning, check out the SciML Open Source Software Organization <https://sciml.ai>.
+function weave_folder(folder, build_list = (:script, :github))
+    weave_files = String[]
+    priorities = Int[]
+    for file in readdir(folder)
+        # Skip non-`.jmd` files
+        endswith(file, ".jmd") || continue
+        push!(weave_files, file)
+        weave_doc = Weave.WeaveDoc(joinpath(folder, file))
+        push!(priorities, get(weave_doc.header, "priority", 0))
+    end
 
-    """)
+    weave_files = weave_files[sortperm(priorities; rev = true)]
+
+    for file in weave_files
+        try
+            @eval @subprocess begin
+                using SciMLBenchmarks
+                SciMLBenchmarks.weave_file($folder, $file, $build_list)
+            end
+        catch e
+            @show folder, file
+            @error(e)
+        end
+    end
+end
+
+function bench_footer(folder = nothing, file = nothing)
+    display(
+        md"""
+## Appendix
+
+These benchmarks are a part of the SciMLBenchmarks.jl repository, found at: <https://github.com/SciML/SciMLBenchmarks.jl>.
+For more information on high-performance scientific machine learning, check out the SciML Open Source Software Organization <https://sciml.ai>.
+
+""",
+    )
     if folder !== nothing && file !== nothing
         display(Markdown.parse("""
         To locally run this benchmark, do the following commands:
@@ -122,8 +124,8 @@ function bench_footer(folder=nothing, file=nothing)
     Package Information:
     """)
 
-    proj = sprint(io -> Pkg.status(io=io))
-    mani = sprint(io -> Pkg.status(io=io, mode = Pkg.PKGMODE_MANIFEST))
+    proj = sprint(io -> Pkg.status(io = io))
+    mani = sprint(io -> Pkg.status(io = io, mode = Pkg.PKGMODE_MANIFEST))
 
     md = """
     ```
@@ -142,11 +144,11 @@ end
 function open_notebooks()
     Base.eval(Main, Meta.parse("import IJulia"))
     weave_all((:notebook,))
-    path = joinpath(repo_directory,"notebook")
-    IJulia.notebook(;dir=path)
-    newpath = joinpath(pwd(),"generated_notebooks")
+    path = joinpath(repo_directory, "notebook")
+    IJulia.notebook(; dir = path)
+    newpath = joinpath(pwd(), "generated_notebooks")
     mv(path, newpath)
-    IJulia.notebook(;dir=newpath)
-  end
+    IJulia.notebook(; dir = newpath)
+end
 
 end # module SciMLBenchmarks
