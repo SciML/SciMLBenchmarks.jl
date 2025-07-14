4ab2f11eabcca3348d057aab84bd772edd3a311f
Arnav Kapoor <kapoorarnav43@gmail.com>
2025-07-14 06:52:56 +0530

    Update make.jl

--- docs/make.jl
+++ docs/make.jl
@@ -8,20 +8,18 @@ dir = @__DIR__() * "/.."
 include("pages.jl")
 
 makedocs(
-    sitename = "The SciML Benchmarks",
-    authors = "Chris Rackauckas",
-    modules = [SciMLBenchmarksOutput],
-    clean = true,
-    doctest = false,
-    format = Documenter.HTML(#analytics = "UA-90474609-3",
-        assets = ["assets/favicon.ico"],
-        canonical = "https://benchmarks.sciml.ai/stable/",
-    ),
-    pages = pages,
+    sitename="The SciML Benchmarks",
+    authors="Chris Rackauckas",
+    modules=[SciMLBenchmarksOutput],
+    clean=true, doctest=false,
+    format=Documenter.HTML(#analytics = "UA-90474609-3",
+        assets=["assets/favicon.ico"],
+        canonical="https://benchmarks.sciml.ai/stable/"),
+    pages=pages
 )
 
 deploydocs(;
-    repo = "github.com/SciML/SciMLBenchmarksOutput",
-    devbranch = "main",
-    branch = "main",
+    repo="github.com/SciML/SciMLBenchmarksOutput",
+    devbranch="main",
+    branch="main"
 )
